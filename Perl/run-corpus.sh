7074de1fb114cc134c60bea17351c14a740a458b
Damian Gryski <dgryski@gmail.com>
2016-08-31 11:29:38 +0200

    change run-corpus for nicer workflow

--- Perl/run-corpus.sh
+++ Perl/run-corpus.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 # This should probably be replaced with a more intelligent perl script
 
 perlbin=perl5.18.4
@@ -19,15 +17,16 @@ function run_perl {
     $perlbin $perlarg run-one.pl <$1
 }
 
-runner=run_perl
-
 case "$1" in
     "gdb") runner=run_gdb ;;
     "valgrind" ) runner=run_valgrind ;;
 esac
 
-echo "using runner $runner"
-
 for i in corpus/5.18/* ; do
-    $runner $i
+    if run_valgrind $i ]; then
+        echo $i pass
+    else
+        run_gdb $i
+        exit
+    fi
 done
